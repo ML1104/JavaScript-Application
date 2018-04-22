a2a87959a6018b572ca6f0f53d76a0cb24dfeb78
Milos <miloske1104@gmail.com>
2018-04-22 03:00:28 +0200

    Form Validation done

--- scripts/domain/main.js
+++ scripts/domain/main.js
@@ -1,28 +1,67 @@
-'use strict';
 
-agCookie.create('example-cookie', true, 1);
 
-var readValue = agCookie.read('example-cookie');
-console.log(readValue);
 
-agCookie.erase('example-cookie');
-'use strict';
 
-console.log('I have entered this file.');
-console.log('This is crystal clear evidence that this works.');
 
-var calculation = 200 + 223;
 
-console.log('Sanity Check: 200 + 223 = ' + calculation);
-console.log('bye.');
-"use strict";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function formValidate() {
-	var emailField = document.forms["contact"]["emailfield"].value;
-	var phoneField = document.forms["contact"]["phonefield"].value;
-	var nameField = document.forms["contact"]["namefield"].value;
-	var surnameField = document.forms["contact"]["surnamefield"].value;
-	var messageField = document.forms["contact"]["messagefield"].value;
+	const emailField = document.forms["contact"]["emailfield"].value;
+	const phoneField = document.forms["contact"]["phonefield"].value;
+	const nameField = document.forms["contact"]["namefield"].value;
+	const surnameField = document.forms["contact"]["surnamefield"].value;
+	const messageField = document.forms["contact"]["messagefield"].value;
 
 	if (emailField == "" || emailField == null) {
 		alert("Email must be filled out");
@@ -52,7 +91,7 @@ function formValidate() {
 	if (!isNaN(surnameField)) {
 		alert("Surname cannot contain numbers");
 		return false;
-	}
+	} 
 
 	if (messageField == "" || messageField == null) {
 		alert("Message cannot be empty");
